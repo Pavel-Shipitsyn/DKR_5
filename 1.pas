c86bc0d269f4e14865abb79aab9211a720e05780
Pavel-Shipitsyn <117993255+Pavel-Shipitsyn@users.noreply.github.com>
2022-12-18 12:22:10 +0100

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,24 @@
+﻿uses crt;
+const
+  n = 10; 
+var
+  a:array[1..n] of integer;
+  i,j,buf:integer;
+begin
+ write('Массив: ');
+  for i:=1 to n do
+      readln(a[i]);
+ 
+  for i:=1 to n-1 do
+    for j:=i+1 to n do
+      if a[i]>a[j] then
+        begin
+          buf:=a[i];
+          a[i]:=a[j];
+          a[j]:=buf;
+        end;
+  writeln;
+  writeln('Отсортированный массив: ');
+  for i:=1 to n do
+    write(a[i],' ');
+end.
---
+++ 2.pas
@@ -0,0 +1,57 @@
+﻿uses crt;
+var a,b,c:array[1..500] of integer;
+    n,m,i,j,k:integer;
+begin
+clrscr;
+write('n=');readln(n);
+write('m=');readln(m);
+writeln('Massiv A:');
+for i:=1 to n do
+   begin
+    a[i]:=2*i+1;
+    write(a[i],' ');
+   end;
+writeln;
+writeln('Massiv B:');
+for i:=1 to m do
+   begin
+    b[i]:=2*i;
+    write(b[i],' ');
+   end;
+writeln;
+i:=1; j:=1; k:=1; 
+while (i<=n) or (j<=m) do 
+  begin
+   if (i<=n) and (j<=m) then 
+     begin
+       if a[i]<b[j] then 
+        begin
+         c[k]:= a[i];
+         inc (i); 
+         inc (k); 
+        end
+       else
+        begin
+         c[k]:= b[j];
+         inc (j);
+         inc (k);
+        end
+     end
+   else if j>m then 
+     begin 
+       c[k]:= a[i];
+       inc (i);
+       inc (k);
+     end
+   else if i>n then
+     begin
+       c[k]:= b[j];
+       inc (j);
+       inc (k);
+     end;
+  end;
+writeln('Massiv C:');
+for i:=1 to m+n do
+write(c[i],' ');
+readln
+end.
